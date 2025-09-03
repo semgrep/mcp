85ab31b87e538fd3ea1af2a125de1043114050ba
Drew Dennison <drew@drewdennison.com>
2025-09-03 14:45:46 -0700

    Remove SEMGREP_SCAN_RPC_DISABLED from server.py

--- src/semgrep_mcp/server.py
+++ src/semgrep_mcp/server.py
@@ -1098,7 +1098,6 @@ TOOL_DISABLE_ENV_VARS = {
     "SEMGREP_FINDINGS_DISABLED": "semgrep_findings",
     "SEMGREP_SCAN_WITH_CUSTOM_RULE_DISABLED": "semgrep_scan_with_custom_rule",
     "SEMGREP_SCAN_DISABLED": "semgrep_scan",
-    "SEMGREP_SCAN_RPC_DISABLED": "semgrep_scan_rpc",
     "SEMGREP_SCAN_LOCAL_DISABLED": "semgrep_scan_local",
     "SECURITY_CHECK_DISABLED": "security_check",
     "GET_ABSTRACT_SYNTAX_TREE_DISABLED": "get_abstract_syntax_tree",
