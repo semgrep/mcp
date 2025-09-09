b5ce3db24c2cde334f6c7d672d7e11628f14a1c2
Katrina Liu <katrina@semgrep.com>
2025-09-09 15:34:50 -0700

    ruff

--- src/semgrep_mcp/server.py
+++ src/semgrep_mcp/server.py
@@ -56,17 +56,18 @@ LOCAL_CODE_FILES_FIELD = Field(
     description=("List of dictionaries with 'path' pointing to the absolute path of the code file")
 )
 
+
 CONFIG_FIELD = Field(
     description="Optional Semgrep configuration string (e.g. 'p/docker', 'p/xss', 'auto')",
     default=None,
 )
 GIT_USERNAME_FIELD = Field(
-    description='''Git username. Use `git config user.name` to get the username. 
-    If unable to get the username, set this to None.'''
+    description="""Git username. Use `git config user.name` to get the username. 
+    If unable to get the username, set this to None."""
 )
 GIT_REPO_FIELD = Field(
-    description='''Git repository. Use `git config remote.origin.url` to get the repository. 
-    If unable to get the repository, set this to None.'''
+    description="""Git repository. Use `git config remote.origin.url` to get the repository. 
+    If unable to get the repository, set this to None."""
 )
 
 RULE_FIELD = Field(description="Semgrep YAML rule string")
